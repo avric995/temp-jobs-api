9b2d07d1126374a86e74bec39bcafda408d6896d
avric995 <avricmiladin995@gmail.com>
2023-05-17 15:48:13 +0200

    first commit

---
+++ middleware/authentication.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
+
+const auth = (req, res, next) => {
+  //check header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    ///// **** DRUGI NACIN
+    // const user = await User.findById({ _id: payload.userId }).select(
+    //   '-password'
+    // );
+    // console.log(user);
+    // req.user = user;
+
+    ///// **** PRVI NACIN
+    req.user = { userId: payload.userId, name: payload.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+};
+
+module.exports = auth;
